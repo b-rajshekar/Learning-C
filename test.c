69da07773154eff086d9b99eae7152e498a2d2a7
Bujjagowns Rajashekhar <91706770+b-rajshekar@users.noreply.github.com>
2023-06-02 14:00:45 +0530

    HelloWorld Program

---
+++ test.c
@@ -0,0 +1,8 @@
+#include <stdio.h> /* it is a header file library that lets us work with input and output functions, such as printf() Hence header files add functionality to C programs.*/
+
+int main() { /* This is called a function, Any code inside its curly brackets {} will be executed.*/
+
+    printf("Hello World"); /* it is a function used to output/print text to the screen.*/
+    
+    return 0; /* This ends the main() function.*/
+}
