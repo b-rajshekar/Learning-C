5677a57678a6f4b0f80ef017c0cc99735a7082b9
Bujjagowns Rajashekhar <91706770+b-rajshekar@users.noreply.github.com>
2023-06-03 10:48:09 +0530

    Add files via upload

---
+++ variables.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    // Student data
+    int studentRollnumber = 1999;
+    int studentAge = 23;
+    float studentFee = 75.25;
+    char studentGrade = 'A';
+
+    // Print variables
+    printf("Student id: %d\n", studentRollnumber);
+    printf("Student age: %d\n", studentAge);
+    printf("StudentFee: %f\n", studentFee);
+    printf("StudentGrade: %c\n", studentGrade);
+    
+    return 0;
+}
