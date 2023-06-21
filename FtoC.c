3a74795b730c280057bd48eb759d9ba0a66f6617
Bujjagowns Rajashekhar <91706770+b-rajshekar@users.noreply.github.com>
2023-06-21 09:34:18 +0530

    Update FtoC.c

--- FtoC.c
+++ FtoC.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main() {
     int f,c;
-
+    
     printf("Please Enter the Fahrenheit: ");
-    scanf("%d", &f);
+    scanf("%d", &f); //takes input from the user and stores in f
     c = (f -32)/1.8;
     printf("%d Fahrenheit is %d celsius \n",
     f,c);
