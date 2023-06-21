acf6546d490f72f5343163284c418eabe3ae2fa9
Bujjagowns Rajashekhar <91706770+b-rajshekar@users.noreply.github.com>
2023-06-21 10:03:20 +0530

    Update miles2.c

--- miles2.c
+++ miles2.c
@@ -4,9 +4,9 @@ int main() {
     double kilometers;
 
     printf("Enter miles space yards: ");
-    scanf("%d%d", &miles, &yards);
+    scanf("%d%d", &miles, &yards); // takes input from the user and stores in miles and yards;
 
-    kilometers = 1.609 * (miles + yards/1760.0);
+    kilometers = 1.609 * (miles + yards/1760.0); // it is a mathematical calculation in C that converts a distance given in miles and yards into kilometers
     printf("\nA marathon is %lf kilometers.\n", kilometers);
 
     return 0;
